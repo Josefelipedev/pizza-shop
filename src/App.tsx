2b7f0adde324c29b2f302fbf829d035a3265df13
Jose Felipe Almeida dos Santos <80927089+Josefelipedev@users.noreply.github.com>
2025-04-16 09:09:11 -0300

    feat:add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router';
 import { Toaster } from 'sonner';
 
+import { ThemeProvider } from '@/components/theme/theme-provider.tsx';
 import { router } from '@/routes.tsx';
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizzashop" defaultTitle="Pizzashop" />
-      <Toaster richColors position="top-right" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | Pizzashop" defaultTitle="Pizzashop" />
+        <Toaster richColors position="top-right" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
---
+++ src/components/account-menu/index.tsx
@@ -0,0 +1,44 @@
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import { Building, ChevronDown, LogOut } from 'lucide-react';
+
+import { Button } from '@/components/ui/button.tsx';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu.tsx';
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 select-none"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Jose Felipe</span>
+          <span className="text-muted-foreground text-xs font-medium">
+            YbGd1@example.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
