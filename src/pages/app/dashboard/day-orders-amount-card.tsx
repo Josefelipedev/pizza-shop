547718d86c930e7c5309680b3574778e328d9434
Jose Felipe Almeida dos Santos <80927089+Josefelipedev@users.noreply.github.com>
2025-05-02 09:35:34 -0300

    feat:dashboard api

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
 import { Utensils } from 'lucide-react';
 
+import { getDayOrdersAmount } from '@/api/get-day-orders-amount.ts';
 import {
   Card,
   CardContent,
@@ -8,6 +10,10 @@ import {
 } from '@/components/ui/card.tsx';
 
 export function DayOrdersAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryFn: getDayOrdersAmount,
+    queryKey: ['metrics', 'day-orders-amount'],
+  });
   return (
     <>
       <Card>
@@ -18,11 +24,26 @@ export function DayOrdersAmountCard() {
           <Utensils className="text-muted-foreground h-4 w-4" />
         </CardHeader>
         <CardContent className="space-y-1">
-          <span className="text-2xl font-bold tracking-tight"> 15</span>
-          <p className="text-muted-foreground text-xs">
-            <span className="text-rose-500 dark:text-rose-400">-2%</span> em
-            relação ao dia passado
-          </p>
+          {dayOrdersAmount && (
+            <>
+              <span className="text-2xl font-bold tracking-tight">
+                {' '}
+                {dayOrdersAmount.amount.toLocaleString('pt-BR')}
+              </span>
+              <p className="text-muted-foreground text-xs">
+                {dayOrdersAmount.diffFromYesterday >= 0 ? (
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    {dayOrdersAmount.diffFromYesterday}%
+                  </span>
+                ) : (
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {dayOrdersAmount.diffFromYesterday}%
+                  </span>
+                )}
+                em relação ao dia passado
+              </p>
+            </>
+          )}
         </CardContent>
       </Card>
     </>
